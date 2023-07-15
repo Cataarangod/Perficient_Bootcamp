8945e26f1e7f1e8cc5fc9d58899f7c09028a0a0c
Catalina Arango <75914408+Cataarangod@users.noreply.github.com>
2023-07-15 15:19:59 -0500

    debug

--- Discoteque.API/Controllers/WeatherForecastController.cs
+++ Discoteque.API/Controllers/WeatherForecastController.cs
@@ -18,15 +18,24 @@ public class WeatherForecastController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    // [HttpGet(Name = "GetWeatherForecast")]
+    // public IEnumerable<WeatherForecast> Get()
+    // {
+    //     return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+    //     {
+    //         Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+    //         TemperatureC = Random.Shared.Next(-20, 55),
+    //         Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+    //     })
+    //     .ToArray();
+    // }
+
+    [HttpGet]
+    public async Task<ActionResult> WomenWhoCode()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        var name = "My name is Cata ";
+        name += "and I like rock n roll";
+        return Ok(name);
     }
+    
 }
